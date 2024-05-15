304f67cabbd802b0c02ef38413627f442d595cbc
martinzutelman <zutelmanmartin@gmail.com>
2024-05-14 22:34:28 -0300

    Added log/sign in with google button with icon.

--- src/components/signin/popup.tsx
+++ src/components/signin/popup.tsx
@@ -2,12 +2,15 @@ import React, { useState } from 'react';
 import RegisterForm from './registerpop';
 import { signIn, useSession } from "next-auth/react";
 import popupCenter from './auth/windowpopup';
+import { RxCross2 } from "react-icons/rx";
+import { FcGoogle } from "react-icons/fc";
 
 interface PopupProps {
   onClose: () => void;
 }
 
 const Popup: React.FC<PopupProps> = ({ onClose }) => {
+  
   const [showRegisterForm, setShowRegisterForm] = useState<boolean>(false);
   const [preferredAgeRange, setPreferredAgeRange] = useState<[number, number]>([18, 35]);
 
@@ -28,15 +31,20 @@ const Popup: React.FC<PopupProps> = ({ onClose }) => {
   return (
     <>
       <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
-        <div className="bg-maincolor rounded-lg p-8 text-center relative">
-          <h2 className="text-2xl font-bold mb-4 text-white">Login/Register</h2>
-          <div>
-            <button onClick={() => {setShowRegisterForm(true); popupCenter("google-signin", "Sign In")}} className="bg-black text-white rounded-md px-4 py-2 mb-2 hover:bg-gray-800 mr-4">Google</button>
-            <button onClick={openRegisterForm} className="bg-black text-white rounded-md px-4 py-2 mb-2 hover:bg-gray-800">Apple</button>
-          </div>
-          <button onClick={onClose} className="absolute top-2 right-2 text-gray-600">
-            Close
-          </button>
+
+          <div className="bg-maincolor rounded-lg p-8 text-center relative flex flex-col">
+
+            <h2 className="text-2xl font-bold mb-4 text-white">Login/Register</h2>
+            
+
+            <button onClick={() => {setShowRegisterForm(true); popupCenter("google-signin", "Sign In")}} className="bg-black text-white rounded-md px-4 py-2 mb-2 hover:bg-gray-800  flex items-center justify-center">
+              <FcGoogle /> <span className='ml-2 pb-[1px]'>Google</span> 
+            </button>
+           
+             
+            <button onClick={onClose} className="absolute top-2 left-2 text-coolred ">
+              <RxCross2 />
+            </button>
         </div>
       </div>
       {showRegisterForm && (
