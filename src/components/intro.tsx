4141a04a4cf30b400177a47cf56543d004f22442
martinzutel <martinzutel@gmail.com>
2024-05-20 16:46:15 -0300

    Started adapting layouts and sizes.

--- src/components/intro.tsx
+++ src/components/intro.tsx
@@ -5,34 +5,44 @@ const Intro: React.FC = () => {
   
 
   return (
-    <div className="flex flex-col">
+    <div>
       <div className="flex items-center justify-center">
-        <div className="h-[750px] w-[2000px] bg-maincolor flex items-center justify-center px-[155px] self-center">
-          <div className="h-[650px] w-[1000px] pt-[100px] pl-[60px]">
-            <p className="font-sofia-pro font-thin text-9xl text-white mb-[50px]">
+
+        <div className="h-[500px] w-[2000px] bg-maincolor flex items-center justify-center px-[155px] self-center">
+         
+          <div className="w-[600px] h-full pl-[80px] pt-[40px]">
+            <p className="font-sofia-pro font-thin text-9xl text-white mb-[20px]">
               domus
             </p>
-            <p className="font-sofia-pro font-bold text text-5xl text-white pr-[100px]">
-              encontra tu lugar, tu hogar, tu espacio, tu lugar en el mundo
-              encontra tu lugar, tu hogar, tu espacio, tu lugar en el mundo
-              encontra tu lugar, tu hogar, tu espacio, tu lugar en el mundo
+            <p className="font-sofia-pro font-bold text text-5xl text-white ">
+              Encontra el mejor host de espacios compartidos, conoce y matchea con la persona ideal. 
             </p>
           </div>
 
-          <Image
-            src="/vectors/House bookshelves-amico.svg"
-            alt=""
-            width={500}
-            height={500}
-            objectPosition="center"
-            quality={100}
-          />
+          <div className='w-[600px] h-full p-[40px]'>
+
+            <div className='w-full h-full relative p-[100px]'>
+              <Image
+                src="/vectors/House bookshelves-amico.svg"
+                alt=""
+                layout='fill'
+                objectPosition="center"
+                quality={100}
+              />
+            </div>
+
+          </div>
+
+          
+
+          
+
         </div>
       </div>
 
       <ParentComponent /> 
 
-     
+
     </div>
   );
 };
--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -4,15 +4,20 @@ import { BsFillPersonFill } from "react-icons/bs";
 const Searchbar = () => {
 
     return (
-        <div className='w-full h-[52px] flex justify-center items-center'>
-        <div className='rounded-l-full h-[58px] w-[280px]'>
-            <input type="text" placeholder='Search' className='h-[58px] w-[280px] bg-darkgre placeholder-white outline-none rounded-l-full pl-4'/>
-        </div>
+    <div className='w-full flex justify-center items-center'>
 
-        <button className='h-[58px] w-[65px] bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre'> <BsFillPersonFill/> </button>
-        <button className='h-[58px] w-[65px] bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre'> <BsFillPersonFill/>  </button>
-        <button className='h-[58px] w-[72px] bg-darkgre rounded-r-full text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre'> <BsFillPersonFill className='mr-[5px]'/> </button>
-    </div>
+        <div className='sm:h-[40px] w-[350px] flex justify-center items-center '>
+ 
+        
+            <input type="text" placeholder='Search' className='h-full w-full bg-darkgre placeholder-white outline-none rounded-l-full pl-4'/>
+            <button className='h-full  bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre'> <BsFillPersonFill className='m-[10px]'/> </button>
+            <button className='h-full  bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre'> <BsFillPersonFill className='m-[10px]'/>  </button>
+            <button className='h-full  bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre rounded-r-full'> <BsFillPersonFill className='ml-[5px] mr-[10px]'/> </button>
+            
+
+        </div>
+        
+       </div>
     )
 
    
