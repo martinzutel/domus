2047abb0d2774328ce6830997db46d71f69e9674
martinzutelman <zutelmanmartin@gmail.com>
2024-05-26 19:11:45 -0300

    Modified build to include prisma.

--- src/components/searchbar.tsx
+++ src/components/searchbar.tsx
@@ -4,19 +4,19 @@ import { BsFillPersonFill } from "react-icons/bs";
 const Searchbar = () => {
 
     return (
-    <div className='w-full flex justify-center items-center'>
+        <div className='w-full flex justify-center items-center'>
 
-        <div className='h-[40px] w-[350px] flex justify-center items-center '>
- 
-        
-            <input type="text" placeholder='Search' className='h-full w-full bg-darkgre placeholder-white outline-none rounded-l-full pl-4'/>
-            <button className='h-full  bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre'> <BsFillPersonFill className='m-[10px]'/> </button>
-            <button className='h-full  bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre'> <BsFillPersonFill className='m-[10px]'/>  </button>
-            <button className='h-full  bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre rounded-r-full'> <BsFillPersonFill className='ml-[5px] mr-[10px]'/> </button>
+            <div className='h-[40px] w-[350px] flex justify-center items-center '>
+    
             
+                <input type="text" placeholder='Search' className='h-full w-full bg-darkgre placeholder-white outline-none rounded-l-full pl-4'/>
+                <button className='h-full  bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre'> <BsFillPersonFill className='m-[10px]'/> </button>
+                <button className='h-full  bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre'> <BsFillPersonFill className='m-[10px]'/>  </button>
+                <button className='h-full  bg-darkgre text-4xl flex justify-center items-center text-coolred hover:bg-coolred hover:text-darkgre rounded-r-full'> <BsFillPersonFill className='ml-[5px] mr-[10px]'/> </button>
+                
 
-        </div>
-        
+            </div>
+            
        </div>
     )
 
