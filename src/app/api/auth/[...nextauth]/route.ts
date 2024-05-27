4283e66e01d3853ab9e90d01569ecc3cf6a7e105
Laok <68285766+Laok0354@users.noreply.github.com>
2024-05-27 17:25:59 -0300

    image res callback (no anda lmao)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,12 +4,29 @@ import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "@prisma/prisma";
 
 const handler = NextAuth({
-  providers: [GoogleProvider({
-    clientId: process.env.GOOGLE_CLIENT_ID as string,
-    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-  })
-],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    })
+  ],
   adapter: PrismaAdapter(prisma),
+  callbacks: {
+    async session({ session, user }) {
+      if (session.user?.image) {
+        // Modify the image URL to request a higher resolution image
+        session.user.image = session.user.image.replace("=s96-c", "=s400-c");
+      }
+      return session;
+    },
+    async signIn({ profile }) {
+      if (profile?.image) {
+        // Modify the image URL to request a higher resolution image
+        profile.image = profile.image.replace("=s96-c", "=s400-c");
+      }
+      return true;
+    }
+  }
 });
 
-export {handler as GET, handler as POST}
+export { handler as GET, handler as POST };
