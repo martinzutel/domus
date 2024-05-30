20c77ad12dedf0eea4ff19999f6b2f639c489acf
BEADE ARRIETA AGUST�N SANTINO <agusbea05@gmail.com>
2024-05-30 10:12:00 -0300

    resizeo de imagenes (ahora si anda)

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -8,41 +8,46 @@ const handler = NextAuth({
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    })
+    }),
   ],
   adapter: PrismaAdapter(prisma),
   callbacks: {
     async session({ session }) {
       if (session.user?.image) {
-        console.log("image is: " + session.user.image);
-        // Modify the image URL to request a higher resolution image
+        // console.log("image is: " + session.user.image);
         session.user.image = session.user.image.replace("=s96-c", "=s400-c");
-        console.log("replaced with: " + session.user.image);
-        prisma.user.update({
-          where: { email: session.user.email },
-          data: {
-            image: session.user.image.replace("=s96-c", "=s400-c")
-          }
-        })
+        // console.log("replaced with: " + session.user.image);
+        if (session.user.email) {
+          await prisma.user.update({
+            where: { email: session.user.email },
+            data: {
+              image: session.user.image.replace("=s96-c", "=s400-c"),
+            },
+          });
+        }
       }
       return session;
     },
     async signIn({ user }) {
       if (user?.image) {
-        console.log("image is: " + user.image);
-        // Modify the image URL to request a higher resolution image
-        user.image = user.image.replace("=s96-c", "=s400-c");
-        console.log("replaced with: " + user.image);
-        prisma.user.update({
-          where: { email: user.email },
-          data: {
-            image: user.image.replace("=s96-c", "=s400-c")
-          }
-        })
+        // console.log("image is: " + user.image);
+        user.image = user.image.replace("=s96-c", "=s480-c");
+        //console.log("replaced with: " + user.image);
+        if (user.email) {
+          await prisma.user.update({
+            where: { email: user.email },
+            data: {
+              image: user.image,
+            },
+          });
+          // console.log ("updated image:" + JSON.stringify(await prisma.user.findMany({
+          //   where: {email: user.email}
+          // })))
+        }
       }
       return true;
-    }
-  }
+    },
+  },
 });
 
 export { handler as GET, handler as POST };
