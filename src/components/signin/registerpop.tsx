c6367fe4ee9bc7dc953a42fc8b27878d89e1669d
ZUTELMAN MARTIN <47572368@ort>
2024-07-08 15:41:32 -0300

    Added own interests form to register form
    Its not working yer.

--- src/components/signin/registerpop.tsx
+++ src/components/signin/registerpop.tsx
@@ -1,6 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { RxCross2 } from "react-icons/rx";
 import { useSession } from 'next-auth/react';
+import InterestModal from '../interests/interestModal';
+import InterestsForm from '../interests/form';
 
 interface FormData {
   age: number;
@@ -16,6 +18,23 @@ interface RegisterFormProps {
 }
 
 const RegisterForm: React.FC<RegisterFormProps> = ({ isOpen, onClose, onSubmit }) => {
+
+  useEffect(() => {
+    // Add the class to the body to block scrolling
+    document.body.classList.add('overflow-hidden');
+    
+    // Cleanup function to remove the class when the component is unmounted
+    return () => {
+        document.body.classList.remove('overflow-hidden');
+    };
+}, []);
+
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const toggleModal = (): void => {
+      setIsModalOpen(!isModalOpen);
+  };
+
   const { data: session } = useSession();
 
   const [formData, setFormData] = useState<FormData>({
@@ -76,11 +95,13 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ isOpen, onClose, onSubmit }
   if (!isOpen) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center">
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50 ">
       <div className="bg-maincolor rounded-lg p-8 text-center relative">
+
         <button onClick={onClose} className="absolute top-2 left-2 text-coolred">
           <RxCross2 />
         </button>
+
         <h2 className="text-2xl font-bold mb-4 text-white">Register</h2>
         <form onSubmit={handleSubmit} className="mb-4">
           <div className="flex-row mb-[16px]">
@@ -155,6 +176,7 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ isOpen, onClose, onSubmit }
               className="border rounded-md p-2 accent-coolred bg-darkgre text-white w-[100%] h-[50px] border-none"
             />
           </div>
+
           <div className="mb-4">
             <label className="block text-left text-white mb-2">Contact:</label>
             <input
@@ -166,6 +188,42 @@ const RegisterForm: React.FC<RegisterFormProps> = ({ isOpen, onClose, onSubmit }
               required
             />
           </div>
+
+          <button
+            onClick={toggleModal}
+            className='border-solid text-darkgre font-black bg-coolred text-lg pt-[0.28rem] pb-[0.47rem] px-[2rem] rounded-full mr-[0.7rem] font-sofia-pro hover:bg-coolredhl active:bg-coolred mb-3'
+          >
+            Select you interests
+          </button>
+
+          <div className='w-full flex flex-col justify-center items-center'>
+            {isModalOpen && (
+                <InterestModal>
+                    <div className="flex flex-col items-center space-y-4">
+                        <div className='w-full'>
+                            <button
+                                onClick={toggleModal}
+                                className="top-0 left-0 px-4 py-2 text-coolred text-3xl rounded"
+                            >
+                                <RxCross2 />
+                            </button>
+                        </div>
+                       
+
+                        <h1 className='text-4xl text-white font-sofia-pro'
+                        >Search through interests:</h1>
+                        
+                        <InterestsForm 
+                        closeModal={toggleModal} 
+                        modalContext="ownTags" 
+                        />
+                       
+                    </div>
+                </InterestModal>
+            )}
+          </div>
+          
+
           <button type="submit" className='border-solid text-darkgre font-black bg-coolred text-lg pt-[0.28rem] pb-[0.47rem] px-[2rem] rounded-full mr-[0.7rem] font-sofia-pro hover:bg-coolredhl active:bg-coolreddrk'>
             Register
           </button>
