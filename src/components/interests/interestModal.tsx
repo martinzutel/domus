cef2a619d1642a38659c046e29b354dba475596b
martinzutelman <zutelmanmartin@gmail.com>
2024-07-09 02:16:09 -0300

    lots of changes
    made modal better, cleaned interest selector in register form, made checkbox component reusable and not a stiff form and more im too eepy

--- src/components/interests/interestModal.tsx
+++ src/components/interests/interestModal.tsx
@@ -1,22 +1,24 @@
 'use client'
 
 import React from 'react';
-import { useEffect } from 'react';
+import { useEffect, ReactNode } from 'react';
+import { RxCross2 } from 'react-icons/rx';
 
 interface InterestModalProps {
-    children: React.ReactNode;
+    children: ReactNode;
+    onClose: () => void;  // Added onClose prop to handle the close action
 }
 
-const InterestModal: React.FC<InterestModalProps> = ({ children }) => {
+const InterestModal: React.FC<InterestModalProps> = ({ children, onClose }) => {
     
     useEffect(() => {
-        // Add the class to the body to block scrolling
+       
         document.body.classList.add('overflow-hidden');
-        
-        // Cleanup function to remove the class when the component is unmounted
+ 
         return () => {
             document.body.classList.remove('overflow-hidden');
         };
+
     }, []);
 
     return (
@@ -24,11 +26,25 @@ const InterestModal: React.FC<InterestModalProps> = ({ children }) => {
         <>
             <div>
                 
-                <div className='fixed h-screen w-screen bg-black/50 top-0 left-0 '></div>
+                <div
+                    className='fixed h-screen w-screen bg-black/50 top-0 left-0 '
+                    onClick={onClose}
+                >
+                    
+                </div>
 
-                <div className='absolute flex items-center justify-center top-72'>
-                    <div className="absolute w-[500px] h-[500px] bg-maincolor p-[20px] rounded-3xl flex flex-col items-center justify-center space-y-4 z-50">
-                    {children}
+                <div
+                    className='absolute flex items-center justify-center top-[400px]'
+                >
+                    <div className="absolute bg-maincolor min-w-[500px] p-[20px] rounded-3xl flex flex-col items-center justify-center space-y-4 z-50">
+                    <button 
+                        onClick={onClose}  // Close modal when clicking the 'X' button
+                        className='absolute top-2 left-2 text-coolred text-3xl'
+                        aria-label="Close Modal"  // Added aria-label for accessibility
+                    >
+                        <RxCross2 />
+                    </button>
+                        {children}
                     </div>
                 </div>
             
